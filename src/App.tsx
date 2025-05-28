40eda9ee23853a841ee9e48d5b353e6f142ee49f
dmitry <makhanekdmitriy@gmail.com>
2025-05-28 12:53:52 +0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="banner">
+        <div className="gallery-name">Третьяковская галерея</div>
+        <div className="sber-logo">СБЕР</div>
+        <div className="warning">
+          ПЕРЕД ИСПОЛЬЗОВАНИЕМ СТЕНАЛ,
+          <br />
+          ОБЪИКОМЫТЕСЬ С ПРАВИЛАМИ
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
